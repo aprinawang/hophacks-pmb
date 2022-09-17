4883055bca0cbd3709c6012eb0debf29f5ecd2ec
aprinawang <awang100@jhu.edu>
2022-09-17 19:13:18 -0400

    drop down expansion

--- src/pages/Trip/Trip.js
+++ src/pages/Trip/Trip.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Profile from '../../components/Profile/Profile';
 import Receipt from '../../components/Receipt/Receipt';
 import './trip.css';
 import { BsCameraFill } from 'react-icons/bs';
 
-const Trip = () => {
-    const items = [
-        {'id': 0, 'name': 'Wings', 'price': 14},
-        {'id': 1, 'name': 'Steak Sandwich', 'price': 16},
-        {'id': 2, 'name': 'Steak Sandwich', 'price': 16},
-        {'id': 3, 'name': 'Mac and Cheese', 'price': 8},
-        {'id': 4, 'name': 'BBQ Burger', 'price': 15},
-    ]
+const item_arr = [
+    {'id': 0, 'name': 'Wings', 'price': 14},
+    {'id': 1, 'name': 'Steak Sandwich', 'price': 16},
+    {'id': 2, 'name': 'Steak Sandwich', 'price': 16},
+    {'id': 3, 'name': 'Mac and Cheese', 'price': 8},
+    {'id': 4, 'name': 'BBQ Burger', 'price': 15},
+]
 
+const Trip = () => {
+    const [items, setItems] = useState(item_arr);
     let tax = 0;
     let tax_rate;
     let tip = 0;
@@ -24,7 +25,7 @@ const Trip = () => {
         <div className='header'>
             <h1 className='trip-title'>{today.toLocaleDateString()} Trip</h1>
             <span className='camera-icon'>
-                <BsCameraFill color="#FFFFFF" size = {'2em'}/>
+                <BsCameraFill color="#FFFFFF" size = {'2.5em'}/>
             </span>
         </div>
         <div>
