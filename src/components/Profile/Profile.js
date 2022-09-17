eb99a9a733f3bab1614e84f33fcd455225994ade
amy-xing <axing1@jh.edu>
2022-09-17 18:55:58 -0400

    continuing profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,7 +6,7 @@ const Profile = () => {
   return (
 
     <div className = 'profile'>
-      <BsFillPersonFill color = "#FFFFFF"/>
+      <BsFillPersonFill color = "#FFFFFF" size = {'2em'}/>
     </div>
 
   )
